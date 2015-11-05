555dcca66f7010641b681961fe0e6aa24b563aab
Sujesh <sujeshakya@deerwalk.com>
2015-11-05 10:31:47 +0545

    VDM2: Modified serverTest.py to make each test case independent of each other

--- tests/server/serverTest.py
+++ tests/server/serverTest.py
@@ -19,16 +19,36 @@ import requests
 
 URL = 'http://localhost:8000/api/1.0/servers/'
 
-class ServerTest(TestCase):
+class Server(unittest.TestCase):
+    def setUp(self):
+        #Create a server
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        data={'description':'test','hostname':'test','name':'test'}
+        response = requests.post(URL,json=data, headers = headers)
+        if response.status_code==201:
+            self.assertEqual(response.status_code, 201)
+        else:
+            self.assertEqual(response.status_code, 404)
+    def tearDown(self):
+        #Delete the server
+        response= requests.get(URL)
+        value= response.json()
+        if value:
+            serverLength = len(value['servers'])
+            lastServerId =  value['servers'][serverLength-1]['id']
+            print "ServerId to be deleted is " + str(lastServerId)
+            url = URL + str(lastServerId)
+            response = requests.delete(url)
+            self.assertEqual(response.status_code,200)
+        else:
+            print "The Server list is empty"
+
+class ServerTest1(TestCase):
 
     def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
         return app
-
-    # def setUp(self):
-    #     response = requests.get(URL)
-    #     value= response.json()
 
     #ensure GET server list
     def test_01GetServers(self):
@@ -56,7 +76,8 @@ class ServerTest(TestCase):
         self.assertEqual(value['error'],'Host name is required')
         self.assertEqual(response.status_code, 404)
 
-    #ensure servers are added properly
+   #ensure servers are added properly
+
     def test_04CreateServer(self):
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         data={'description':'test','hostname':'test','name':'test'}
@@ -66,52 +87,7 @@ class ServerTest(TestCase):
         else:
             self.assertEqual(response.status_code, 404)
 
-    #ensure Duplicate Server name is not added
-    def test_05ValidateDuplicateServerName(self):
-        response = requests.get(URL)
-        headers = {'Content-Type': 'application/json; charset=utf-8'}
-        data={'description':'test','hostname':'test12345','name':'test'}
-        response = requests.post(URL,json=data, headers = headers)
-        value=response.json()
-        if response.status_code==201:
-            print "new server created"
-            self.assertEqual(response.status_code, 201)
-        else:
-            self.assertEqual(response.status_code, 404)
-            self.assertEqual(value['error'],'Server name already exists')
-            print value['error']
-
-    #ensure Duplicate Host name is not added
-    def test_06ValidateDuplicateHostName(self):
-        headers = {'Content-Type': 'application/json; charset=utf-8'}
-        data={'description':'test','hostname':'test','name':'test12345'}
-        response = requests.post(URL,json=data, headers = headers)
-        value=response.json()
-        if response.status_code==201:
-            self.assertEqual(response.status_code, 201)
-        else:
-            self.assertEqual(response.status_code, 404)
-            self.assertEqual(value['error'],'Host name already exists')
-            print value['error']
-
-     #ensure servers are updated properly
-    def test_07UpdateServers(self):
-        #get a serverId
-        response= requests.get(URL)
-        value= response.json()
-
-        if value:
-            serverLength = len(value['servers'])
-            lastServerId =  value['servers'][serverLength-1]['id']
-            print "ServerId to be updated is " + str(lastServerId)
-            url = URL + str(lastServerId)
-            response = requests.put(url,json={'description':'ttest123'})
-            self.assertEqual(response.status_code,200)
-        else:
-            print "The Server list is empty"
-
-
-    #ensure servers are deleted propery
+    #ensure servers are deleted properly
     def test_08DeleteServer(self):
         #Create a server to delete
         headers = {'Content-Type': 'application/json; charset=utf-8'}
@@ -135,7 +111,50 @@ class ServerTest(TestCase):
         else:
             print "The Server list is empty"
 
+class ServerTest2(Server):
+    #ensure Duplicate Server name is not added
+    def test_09ValidateDuplicateServerName(self):
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        data={'description':'test','hostname':'test12345','name':'test'}
+        response = requests.post(URL,json=data, headers = headers)
+        value=response.json()
+        if response.status_code==201:
+            print "new server created"
+            self.assertEqual(response.status_code, 201)
+        else:
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(value['error'],'Server name already exists')
+            print value['error']
 
+    #ensure Duplicate Host name is not added
+    def test_10ValidateDuplicateHostName(self):
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        data={'description':'test','hostname':'test','name':'test12345'}
+        response = requests.post(URL,json=data, headers = headers)
+        value=response.json()
+        if response.status_code==201:
+            self.assertEqual(response.status_code, 201)
+        else:
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(value['error'],'Host name already exists')
+            print value['error']
+
+     #ensure servers are updated properly
+
+    def test_11UpdateServers(self):
+        #get a serverId
+        response= requests.get(URL)
+        value= response.json()
+
+        if value:
+            serverLength = len(value['servers'])
+            lastServerId =  value['servers'][serverLength-1]['id']
+            print "ServerId to be updated is " + str(lastServerId)
+            url = URL + str(lastServerId)
+            response = requests.put(url,json={'description':'ttest123'})
+            self.assertEqual(response.status_code,200)
+        else:
+            print "The Server list is empty"
 
 
 if __name__ == '__main__':
