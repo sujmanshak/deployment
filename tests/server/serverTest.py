dc6b9a753326daaf3ecc6feebced171c73382b45
Sujesh <sujeshakya@deerwalk.com>
2015-11-10 14:02:09 +0545

    VDM-2: Port settings added to the servers in server side

--- tests/server/serverTest.py
+++ tests/server/serverTest.py
@@ -43,14 +43,15 @@ class Server(unittest.TestCase):
         else:
             print "The Server list is empty"
 
-class ServerTest1(TestCase):
+
+class CreateServer(Server):
 
     def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
         return app
 
-    #ensure GET server list
+     #ensure GET server list
     def test_01GetServers(self):
         response = requests.get(URL)
         value= response.json()
@@ -78,42 +79,8 @@ class ServerTest1(TestCase):
 
    #ensure servers are added properly
 
-    def test_04CreateServer(self):
-        headers = {'Content-Type': 'application/json; charset=utf-8'}
-        data={'description':'test','hostname':'test','name':'test'}
-        response = requests.post(URL,json=data, headers = headers)
-        if response.status_code==201:
-            self.assertEqual(response.status_code, 201)
-        else:
-            self.assertEqual(response.status_code, 404)
-
-    #ensure servers are deleted properly
-    def test_08DeleteServer(self):
-        #Create a server to delete
-        headers = {'Content-Type': 'application/json; charset=utf-8'}
-        data={'description':'test','hostname':'test','name':'test'}
-        response = requests.post(URL,json=data, headers = headers)
-        if response.status_code==201:
-            self.assertEqual(response.status_code, 201)
-        else:
-            self.assertEqual(response.status_code, 404)
-
-        #get a serverId
-        response= requests.get(URL)
-        value= response.json()
-        if value:
-            serverLength = len(value['servers'])
-            lastServerId =  value['servers'][serverLength-1]['id']
-            print "ServerId to be deleted is " + str(lastServerId)
-            url = URL + str(lastServerId)
-            response = requests.delete(url)
-            self.assertEqual(response.status_code,200)
-        else:
-            print "The Server list is empty"
-
-class ServerTest2(Server):
     #ensure Duplicate Server name is not added
-    def test_09ValidateDuplicateServerName(self):
+    def test_04ValidateDuplicateServerName(self):
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         data={'description':'test','hostname':'test12345','name':'test'}
         response = requests.post(URL,json=data, headers = headers)
@@ -127,7 +94,7 @@ class ServerTest2(Server):
             print value['error']
 
     #ensure Duplicate Host name is not added
-    def test_10ValidateDuplicateHostName(self):
+    def test_05ValidateDuplicateHostName(self):
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         data={'description':'test','hostname':'test','name':'test12345'}
         response = requests.post(URL,json=data, headers = headers)
@@ -141,7 +108,36 @@ class ServerTest2(Server):
 
      #ensure servers are updated properly
 
-    def test_11UpdateServers(self):
+class UpdateServer(Server):
+    #ensure GET server list
+    def test_06GetServers(self):
+        response = requests.get(URL)
+        value= response.json()
+        if not value:
+            print "The Server list is empty"
+        self.assertEqual(response.status_code, 200)
+
+    #ensure server name is not empty
+    def test_07ValidateServername(self):
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        data={'description':'test','hostname':'test','name':''}
+        response = requests.post(URL,json=data, headers = headers)
+        value=response.json()
+        self.assertEqual(value['error'],'Server name is required')
+        self.assertEqual(response.status_code, 404)
+
+    #ensure server name is not empty
+    def test_08ValidateHostname(self):
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        data={'description':'test','hostname':'','name':'test'}
+        response = requests.post(URL,json=data, headers = headers)
+        value=response.json()
+        self.assertEqual(value['error'],'Host name is required')
+        self.assertEqual(response.status_code, 404)
+
+   #ensure servers are added properly
+
+    def test_09UpdateServers(self):
         #get a serverId
         response= requests.get(URL)
         value= response.json()
@@ -155,6 +151,147 @@ class ServerTest2(Server):
             self.assertEqual(response.status_code,200)
         else:
             print "The Server list is empty"
+
+    #ensure Duplicate Server name is not added
+    def test_10ValidateDuplicateServerName(self):
+        response = requests.put(URL,json={'description':'test','hostname':'test12345','name':'test'})
+        value=response.json()
+        if response.status_code==201:
+            print "new server created"
+            self.assertEqual(response.status_code, 201)
+        else:
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(value['error'],'Server name already exists')
+            print value['error']
+
+    #ensure Duplicate Host name is not added
+    def test_11ValidateDuplicateHostName(self):
+        response = requests.put(URL,json={'description':'test','hostname':'test','name':'test12345'})
+        value=response.json()
+        if response.status_code==201:
+            self.assertEqual(response.status_code, 201)
+        else:
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(value['error'],'Host name already exists')
+            print value['error']
+
+     #ensure servers are updated properly
+
+# class ServerTest1(TestCase):
+#
+#     def create_app(self):
+#         app = Flask(__name__)
+#         app.config['TESTING'] = True
+#         return app
+#
+#     #ensure GET server list
+#     def test_01GetServers(self):
+#         response = requests.get(URL)
+#         value= response.json()
+#         if not value:
+#             print "The Server list is empty"
+#         self.assertEqual(response.status_code, 200)
+#
+#     #ensure server name is not empty
+#     def test_02ValidateServername(self):
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'test','name':''}
+#         response = requests.post(URL,json=data, headers = headers)
+#         value=response.json()
+#         self.assertEqual(value['error'],'Server name is required')
+#         self.assertEqual(response.status_code, 404)
+#
+#     #ensure server name is not empty
+#     def test_03ValidateHostname(self):
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'','name':'test'}
+#         response = requests.post(URL,json=data, headers = headers)
+#         value=response.json()
+#         self.assertEqual(value['error'],'Host name is required')
+#         self.assertEqual(response.status_code, 404)
+#
+#    #ensure servers are added properly
+#
+#     def test_04CreateServer(self):
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'test','name':'test'}
+#         response = requests.post(URL,json=data, headers = headers)
+#         if response.status_code==201:
+#             self.assertEqual(response.status_code, 201)
+#         else:
+#             self.assertEqual(response.status_code, 404)
+#
+#     #ensure servers are deleted properly
+#     def test_08DeleteServer(self):
+#         #Create a server to delete
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'test','name':'test'}
+#         response = requests.post(URL,json=data, headers = headers)
+#         if response.status_code==201:
+#             self.assertEqual(response.status_code, 201)
+#         else:
+#             self.assertEqual(response.status_code, 404)
+#
+#         #get a serverId
+#         response= requests.get(URL)
+#         value= response.json()
+#         if value:
+#             serverLength = len(value['servers'])
+#             lastServerId =  value['servers'][serverLength-1]['id']
+#             print "ServerId to be deleted is " + str(lastServerId)
+#             url = URL + str(lastServerId)
+#             response = requests.delete(url)
+#             self.assertEqual(response.status_code,200)
+#         else:
+#             print "The Server list is empty"
+#
+# class ServerTest2(Server):
+#     #ensure Duplicate Server name is not added
+#     def test_09ValidateDuplicateServerName(self):
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'test12345','name':'test'}
+#         response = requests.post(URL,json=data, headers = headers)
+#         value=response.json()
+#         if response.status_code==201:
+#             print "new server created"
+#             self.assertEqual(response.status_code, 201)
+#         else:
+#             self.assertEqual(response.status_code, 404)
+#             self.assertEqual(value['error'],'Server name already exists')
+#             print value['error']
+#
+#     #ensure Duplicate Host name is not added
+#     def test_10ValidateDuplicateHostName(self):
+#         headers = {'Content-Type': 'application/json; charset=utf-8'}
+#         data={'description':'test','hostname':'test','name':'test12345'}
+#         response = requests.post(URL,json=data, headers = headers)
+#         value=response.json()
+#         if response.status_code==201:
+#             self.assertEqual(response.status_code, 201)
+#         else:
+#             self.assertEqual(response.status_code, 404)
+#             self.assertEqual(value['error'],'Host name already exists')
+#             print value['error']
+#
+#      #ensure servers are updated properly
+#
+#     def test_11UpdateServers(self):
+#         #get a serverId
+#         response= requests.get(URL)
+#         value= response.json()
+#
+#         if value:
+#             serverLength = len(value['servers'])
+#             lastServerId =  value['servers'][serverLength-1]['id']
+#             print "ServerId to be updated is " + str(lastServerId)
+#             url = URL + str(lastServerId)
+#             response = requests.put(url,json={'description':'ttest123'})
+#             self.assertEqual(response.status_code,200)
+#         else:
+#             print "The Server list is empty"
+
+
+
 
 
 if __name__ == '__main__':
--- vdm/server/HTTPListener.py
+++ vdm/server/HTTPListener.py
@@ -9,21 +9,39 @@ servers = [
         'name': u'voltdb1',
         'hostname': u'voltdb1',
         'description': u'voltdb server 1',
-        'enabled': True
+        'enabled': True,
+        'portname': '21223',
+        'adminport': '21211',
+        'http': '8080',
+        'internalport':'3021',
+        'zookeeper': '2181',
+        'replicationport':'5555'
     },
     {
         'id': 2,
         'name': u'voltdb2',
         'hostname': u'voltdb2',
         'description': u'voltdb server 2',
-        'enabled': True
+        'enabled': True,
+        'portname': '21223',
+        'adminport': '21211',
+        'http': '8080',
+        'internalport':'3021',
+        'zookeeper': '2181',
+        'replicationport':'5555'
     },
     {
         'id': 3,
         'name': u'voltdb3',
         'hostname': u'voltdb3',
         'description': u'voltdb server 3',
-        'enabled': True
+        'enabled': True,
+        'portname': '21223',
+        'adminport': '21211',
+        'http': '8080',
+        'internalport':'3021',
+        'zookeeper': '2181',
+        'replicationport':'5555'
     }
 ]
 
@@ -45,6 +63,7 @@ def make_public_server(servers):
     for field in servers:
         new_server[field] = servers[field]
     return new_server
+
 
 class ServerAPI(MethodView):
 
@@ -77,6 +96,74 @@ class ServerAPI(MethodView):
         if len(server)!=0:
             return make_response(jsonify({'error': 'Host name already exists'}), 404)
 
+        if 'portname' in request.json:
+            if(request.json['portname']!=""):
+                try:
+                    val = int(request.json['portname'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Port name must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Port name must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                        return make_response(jsonify({'error': 'Port name must be a positive number'}), 404)
+
+        if 'adminport' in request.json:
+            if(request.json['adminport']!=""):
+                try:
+                    val = int(request.json['adminport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Admin port must be a positive number'}), 404)
+                    elif val<1 or val>=65535:
+                        return make_response(jsonify({'error': 'Admin port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Admin port must be a positive number'}), 404)
+
+        if 'http' in request.json:
+            if(request.json['http']!=""):
+                try:
+                    val = int(request.json['http'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Http must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Http must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Http must be a positive number'}), 404)
+
+        if 'internalport' in request.json:
+            if(request.json['internalport']!=""):
+                try:
+                    val = int(request.json['internalport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Internal port must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Internal port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Internal port must be a positive number'}), 404)
+
+        if 'zookeeper' in request.json:
+            if(request.json['zookeeper']!=""):
+                try:
+                    val = int(request.json['zookeeper'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Zookeeper must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Zookeeper must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Zookeeper must be a positive number'}), 404)
+
+        if 'replicationport' in request.json:
+            if(request.json['replicationport']!=""):
+                try:
+                    val = int(request.json['replicationport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Replication port must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Replication port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Replication port must be a positive number'}), 404)
+
+
+
         serverId = 0
         if not servers:
             serverId = 1
@@ -87,7 +174,13 @@ class ServerAPI(MethodView):
         'name': request.json['name'],
         'description': request.json.get('description', ""),
         'hostname': request.json.get('hostname', ""),
-        'enabled': True
+        'enabled': True,
+        'portname': request.json.get('portname',""),
+        'adminport': request.json.get('adminport',""),
+        'http': request.json.get('http',""),
+        'internalport': request.json.get('internalport',""),
+        'zookeeper': request.json.get('zookeeper',""),
+        'replicationport': request.json.get('replicationport',"")
         }
         servers.append(server)
         return jsonify( { 'server': server, 'status':1 } ),201
@@ -102,8 +195,9 @@ class ServerAPI(MethodView):
 
     def put(self, server_id):
         # update a single server
-        server = filter(lambda t: t['id'] == server_id, servers)
-        if len(server) == 0:
+
+        currentserver = filter(lambda t: t['id'] == server_id, servers)
+        if len(currentserver) == 0:
             abort(404)
         if not request.json:
             abort(400)
@@ -114,10 +208,98 @@ class ServerAPI(MethodView):
         if 'enabled' in request.json and type(request.json['enabled']) is not bool:
             abort(400)
 
-        server[0]['name'] = request.json.get('name', server[0]['name'])
-        server[0]['description'] = request.json.get('description', server[0]['description'])
-        server[0]['enabled'] = request.json.get('enabled', server[0]['enabled'])
-        return jsonify( { 'server': server[0] } )
+        if 'name' in request.json:
+            if request.json['name']=="":
+                return make_response(jsonify({'error':'Server name is required'}),404)
+            server = filter(lambda t: t['name'] == request.json['name'], servers)
+            if len(server)!=0:
+                return make_response(jsonify({'error': 'Server name already exists'}), 404)
+
+        if 'hostname' in request.json:
+            if request.json['hostname']=="":
+                return make_response(jsonify({'error':'Host name is required'}),404)
+            server = filter(lambda t: t['hostname'] == request.json['hostname'], servers)
+            if len(server)!=0:
+                return make_response(jsonify({'error': 'Host name already exists'}), 404)
+
+        if 'portname' in request.json:
+            if(request.json['portname']!=""):
+                try:
+                    val = int(request.json['portname'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Port name must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Port name must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                        return make_response(jsonify({'error': 'Port name must be a positive number'}), 404)
+
+        if 'adminport' in request.json:
+            if(request.json['adminport']!=""):
+                try:
+                    val = int(request.json['adminport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Admin port must be a positive number'}), 404)
+                    elif val<1 or val>=65535:
+                        return make_response(jsonify({'error': 'Admin port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Admin port must be a positive number'}), 404)
+
+        if 'http' in request.json:
+            if(request.json['http']!=""):
+                try:
+                    val = int(request.json['http'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Http must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Http must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Http must be a positive number'}), 404)
+
+        if 'internalport' in request.json:
+            if(request.json['internalport']!=""):
+                try:
+                    val = int(request.json['internalport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Internal port must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Internal port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Internal port must be a positive number'}), 404)
+
+        if 'zookeeper' in request.json:
+            if(request.json['zookeeper']!=""):
+                try:
+                    val = int(request.json['zookeeper'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Zookeeper must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Zookeeper must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Zookeeper must be a positive number'}), 404)
+
+        if 'replicationport' in request.json:
+            if(request.json['replicationport']!=""):
+                try:
+                    val = int(request.json['replicationport'])
+                    if val<0:
+                        return make_response(jsonify({'error': 'Replication port must be a positive number'}), 404)
+                    elif val<1 or val>65535:
+                        return make_response(jsonify({'error': 'Replication port must be greater than 1 and less than 65535'}), 404)
+                except ValueError:
+                    return make_response(jsonify({'error': 'Replication port must be a positive number'}), 404)
+
+        currentserver[0]['name'] = request.json.get('name', currentserver[0]['name'])
+        currentserver[0]['hostname'] = request.json.get('hostname', currentserver[0]['hostname'])
+        currentserver[0]['description'] = request.json.get('description', currentserver[0]['description'])
+        currentserver[0]['enabled'] = request.json.get('enabled', currentserver[0]['enabled'])
+        currentserver[0]['portname'] = request.json.get('portname', currentserver[0]['portname'])
+        currentserver[0]['adminport'] = request.json.get('adminport', currentserver[0]['adminport'])
+        currentserver[0]['http'] = request.json.get('http', currentserver[0]['http'])
+        currentserver[0]['internalport'] = request.json.get('internalport', currentserver[0]['internalport'])
+        currentserver[0]['zookeeper'] = request.json.get('zookeeper', currentserver[0]['zookeeper'])
+        currentserver[0]['replicationport'] = request.json.get('replicationport', currentserver[0]['replicationport'])
+        return jsonify( { 'server': currentserver[0] } )
+
 
 if __name__ == '__main__':
     app.config.update(
